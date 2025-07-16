a91910e9b9a7af2fea419b3cb88caebaba49f10a
Ghania Shahzad <ghaniashahzad287@gmail.com>
2025-07-17 01:45:08 +0500

    Implement Dark Mode.

---
+++ src/components/Navbar/DarkMode.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from "react";
+import { FiSun, FiMoon } from "react-icons/fi";
+
+const DarkMode = () => {
+  const [theme, setTheme] = useState(
+    localStorage.getItem("theme") || "light"
+  );
+
+  const element = document.documentElement;
+
+  useEffect(() => {
+    if (theme === "dark") {
+      element.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    } else {
+      element.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    }
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
+  return (
+    
+  );
+};
+
+export default DarkMode;
