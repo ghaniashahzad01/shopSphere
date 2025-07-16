debdb565a19677d7c957b6966b6da08879715fca
Ghania Shahzad <ghaniashahzad287@gmail.com>
2025-07-17 02:27:54 +0500

    Installing dependencies.

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+   colors: {
+      primary: '#FF5733',     // example color
+      secondary: '#C70039',
+    },
+  plugins: [],
+}
+
