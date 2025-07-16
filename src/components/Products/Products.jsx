59ea8a827111bbb56f898ea8d3ccae111d6f55ac
Ghania Shahzad <ghaniashahzad287@gmail.com>
2025-07-17 03:14:07 +0500

    Implementing product page.

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -71,7 +71,27 @@ const generateProducts = () => {
   return ProductsData;
 };
 
+const Products = () => {
+  const ProductsData = generateProducts();
+  const [selected, setSelected] = useState(null);
+  const [quantity, setQuantity] = useState(1);
 
+  const settings = {
+    dots: false,
+    arrows: true,
+    infinite: true,
+    slidesToShow: 5,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    slidesToScroll: 2,
+    responsive: [
+      { breakpoint: 1024, settings: { slidesToShow: 3, slidesToScroll: 2 }},
+      { breakpoint: 640, settings: { slidesToShow: 1, slidesToScroll: 1 }},
+    ],
+  };
+
+  
+};
   
 
 
