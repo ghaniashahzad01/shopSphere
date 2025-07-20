7f45171a6f27aeedd7402652d7c9ad36e21a2702
Ghania Shahzad <ghaniashahzad287@gmail.com>
2025-07-20 11:06:33 +0500

    Final commit.

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,48 @@
-import React, { useState, useEffect } from "react";
-import AOS from "aos";
-import "aos/dist/aos.css";
-
+import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import Products from "./components/Products/Products";
+import AOS from "aos";
+import "aos/dist/aos.css";
 import TopProducts from "./components/TopProducts/TopProducts";
 import Banner from "./components/Banner/Banner";
 import Subscribe from "./components/Subscribe/Subscribe";
-import AboutUs from "./components/AboutUs/AboutUs";
 import Testimonials from "./components/Testimonials/Testimonials";
 import Footer from "./components/Footer/Footer";
 import Popup from "./components/Popup/Popup";
+import AboutUs from "./components/AboutUs/AboutUs";
 
 const App = () => {
-  const [orderPopup, setOrderPopup] = useState(false);
+  const [orderPopup, setOrderPopup] = React.useState(false);
 
   const handleOrderPopup = () => {
-    setOrderPopup((prev) => !prev);
+    setOrderPopup(!orderPopup);
   };
-
-  useEffect(() => {
+  React.useEffect(() => {
     AOS.init({
       offset: 100,
       duration: 800,
       easing: "ease-in-sine",
       delay: 100,
     });
-
-    return () => {
-      AOS.refreshHard();
-    };
+    AOS.refresh();
   }, []);
 
   return (
-    <div className="bg-white dark:bg-gray-900 dark:text-gray-200 duration-200">
+    <div className="bg-white dark:bg-gray-900 dark:text-white duration-200">
       <Navbar handleOrderPopup={handleOrderPopup} />
-
       <Hero handleOrderPopup={handleOrderPopup} />
-      <Products handleOrderPopup={handleOrderPopup} />
+      <Products />
       <TopProducts handleOrderPopup={handleOrderPopup} />
       <Banner />
       <Subscribe />
       <AboutUs />
       <Testimonials />
       <Footer />
-
       <Popup orderPopup={orderPopup} setOrderPopup={setOrderPopup} />
     </div>
   );
 };
 
 export default App;
+
